862e57e79b5d776f3968f8dbe8930510121e87d8
Nan-Hung Hsieh <d99622005@ntu.edu.tw>
2022-01-21 21:04:55 -0800

    update

--- make-pdf.R
+++ make-pdf.R
@@ -1,1 +1,10 @@
-rmarkdown::render("nhcv.rmd")
+pkg <- c("tinytex",
+         "rmarkdown")
+for (i in 1:length(pkg)) {
+  tmp <- require(pkg[i], character.only = TRUE)
+  if (!tmp) install.packages(pkg[i])
+  library(pkg[i], character.only = TRUE)
+}
+if(!tinytex::is_tinytex()) tinytex::install_tinytex()
+
+rmarkdown::render("nhcv.Rmd")
